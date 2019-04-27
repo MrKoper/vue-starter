3bca7ef7ba9beda23831b90ab279c2d5d8f2f2d1
student17 <student17@pcoip.ki.agh.edu.pl>
2019-04-27 15:24:00 +0200

    Dodanie komponentów

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,33 @@
     <h1>Witaj w systemie do zapisów na zajęcia</h1>
     <!--<p>{{ email }}</p>-->
 	<div v-if="isAuthenticated">
-		<p>Zalogowany jako {{ email}}</p>
+		<p>Zalogowany jako {{ email }}</p>
     	<button @click="logMeOut()">Wyloguj</button>
     </div>
-    <div v-else>
+    <!-- <div v-else>
     	<input type="text" v-model="email">
     	<button @click="logMeIn()">Zaloguj się</button>
+	</div> -->
+	<div v-else>
+    	<form @submit.prevent="">
+		<login-form @login="logMeIn($event)"
+					header="Zaloguj się e-mailem"
+					button-label="Zaloguj"></login-form>
+		<login-form @login="logMeIn($event)"
+					header="Zarejestruj się e-mailem"
+					button-label="Zarejestruj"></login-form>
+    	</form>
 	</div>
   </div>
 </template>
 
 <script>
+import "milligram";
+import LoginForm from "./LoginForm";
+import LoginPage from "./LoginPage";
+
 export default {
+	components: {LoginForm},
 	data() {
 		  return {
 		    email: 'mwo@agh.pl',
@@ -27,7 +42,8 @@ export default {
 		  }
 	},
 	methods: {
-		logMeIn() {
+		logMeIn(username) {
+			this.email = username;
 			this.isAuthenticated = true;
 		},
 		logMeOut() {
